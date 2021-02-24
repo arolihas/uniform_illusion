cd9eaf05a56856af95fbba8c5833f3df74051503
Sahil Arora <sahil.arora@ibm.com>
2021-02-24 17:06:27 -0500

    updates

--- generate_images.py
+++ generate_images.py
@@ -4,12 +4,10 @@ from PIL import Image, ImageFilter
 from IPython import get_ipython
 
 ipython = get_ipython()
-ipython.magic(load_ext holoviews.ipython)
-ipython.magic(opts Layout [vertical_spacing=0.0 horizontal_spacing=0.0] Image [show_xaxis=None show_yaxis=None show_frame=False show_title=False padding=0])
+ipython.magic("load_ext holoviews.ipython")
+ipython.magic("opts Layout [vertical_spacing=0.0 horizontal_spacing=0.0] Image [show_xaxis=None show_yaxis=None show_frame=False show_title=False padding=0]")
 
 def illusion(pat): 
-    bh, bw = 16, 16
-    pat = pat.resize((bh, bw))
     h, w = 256, 256
     uni = Image.new('RGB', (h,w))
     for i in range(0, w, bw):
@@ -21,7 +19,7 @@ def illusion(pat):
     uni.paste(blur, box)
     return uni
 
-shapes = [ig.Rectangle()]
+shapes = [ig.Rectangle(), ig.RawRectangle(), ig.Ring(), ig.Disk()]
 c = 0
 for shape in shapes: 
     for i in range(-0.95, 0.01, 0.95):
@@ -30,19 +28,24 @@ for shape in shapes:
             shape.y = j
 
             c += 1
-            hv.save(shape[:], 'illusion_image/blur/{}.png'.format(c))
-            pat = Image.open('illusion_image/blur/{}.png'.format(c))
+            hv.save(shape[:], 'illusion_image/blur/uniform/{}.png'.format(c));
+            pat = Image.open('illusion_image/blur/uniform/{}.png'.format(c))
+            pat = pat.resize((16,16))
+            pat.save('illusion_image/blur/uniform/{}.png'.format(c))
             ill_0 = illusion(pat)
-            ill_0.save('illusion_image/blur/{}.png'.format(c))
+            ill_0.save('illusion_image/blur/blurred/{}.png'.format(c))
 
             c += 1
-            pat = pat.resize((16, 16))
             pat = pat.crop((1,1,15,15))
+            pat = pat.resize((16, 16))
+            pat.save('illusion_image/blur/uniform/{}.png'.format(c))
             ill_1 = illusion(pat)
-            ill_1.save('illusion_image/blur/{}.png'.format(c))
+            ill_1.save('illusion_image/blur/blurred/{}.png'.format(c))
 
             c += 1
             pat = pat.resize((100, 100))
             pat = pat.crop((5,5,bh-5,bw-5))
+            pat = pat.resize((16, 16))
+            pat.save('illusion_image/blur/uniform/{}.png'.format(c))
             ill_2 = illusion(pat)
             ill_2.save('illusion_image/blur/{}.png'.format(c))
